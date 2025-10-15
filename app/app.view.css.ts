30ae82ab25f3d9ca54da00396df87261998d6df8
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-10-15 22:24:02 +0300

    clran

--- app/app.view.css.ts
+++ app/app.view.css.ts
@@ -1,26 +1,5 @@
 namespace $.$$ {
 	const { rem } = $mol_style_unit
 
-	$mol_style_define($bog_pay_app, {
-		maxWidth: '100vw',
-		overflowX: 'hidden',
-
-		Head: {
-			display: 'flex',
-			alignItems: 'center',
-			justifyContent: 'space-between',
-			maxWidth: '100%',
-		},
-
-		Games_button: {
-			position: 'absolute',
-			left: '50%',
-			transform: 'translateX(-50%)',
-		},
-
-		Body: {
-			maxWidth: '100vw',
-			overflowX: 'hidden',
-		},
-	})
+	$mol_style_define($bog_pay_app, {})
 }
