87fae76be8df1c996c6120870b9105f3fab3f565
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-10-16 15:22:42 +0300

    theme

---
+++ theme/demo/demo.view.ts
@@ -0,0 +1,11 @@
+namespace $.$$ {
+	export class $bog_pay_theme_demo extends $.$bog_pay_theme_demo {
+		hue_deg() {
+			return this.hue() + 'deg'
+		}
+
+		hue_spread_deg() {
+			return this.hue_spread() + 'deg'
+		}
+	}
+}
