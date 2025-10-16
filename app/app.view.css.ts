3d5889c3e91aaaa236f1d5562babfc5e02c50028
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-10-16 23:05:54 +0300

    css

--- app/app.view.css.ts
+++ app/app.view.css.ts
@@ -1,3 +1,14 @@
 namespace $.$$ {
-	$mol_style_define($bog_pay_app, {})
+	$mol_style_define($bog_pay_app, {
+		Deck: {
+			Switch: {
+				Option: {
+					':first-of-type': {
+						color: 'black',
+					},
+					color: 'aliceblue',
+				},
+			},
+		},
+	})
 }
