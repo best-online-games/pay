fcd4ea71a28fff80fe52fb37ff7f161436292a2d
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-10-16 15:51:45 +0300

    1111

--- app/app.view.css.ts
+++ app/app.view.css.ts
@@ -1,3 +1,7 @@
 namespace $.$$ {
-	$mol_style_define($bog_pay_app, {})
+	$mol_style_define($bog_pay_app, {		// Стили только для главной страницы
+		padding: '0',
+		flex: {
+			grow: 1,
+})
 }
---
+++ app/deck/deck.view.css.ts
@@ -0,0 +1,11 @@
+namespace $.$$ {
+	// Стили для переключателя вкладок
+	$mol_style_define($bog_pay_app_deck, {
+		Switch: {
+			background: {
+				color: '#f5f5f5',
+			},
+			padding: '10px',
+		},
+	})
+}
