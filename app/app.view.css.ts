1c660b0bd2f2ffa5d402dc4b1a29d5b9f00b7921
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-10-17 00:00:52 +0300

    css

--- app/app.view.css.ts
+++ app/app.view.css.ts
@@ -3,18 +3,16 @@ namespace $.$$ {
 		Deck: {
 			Switch: {
 				Option: {
-					':nth-of-type(1)': {
+					':nth-of-type(1)': {},
+					':nth-of-type(2)': {
+						color: 'black',
+					},
+					':nth-of-type(3)': {
 						backgroundImage: 'linear-gradient(135deg, #667eea 0%, #764ba2 100%)',
 						color: 'white',
 						borderRadius: '8px',
 						padding: { top: '12px', right: '24px', bottom: '12px', left: '24px' },
 						fontWeight: '500',
-					},
-					':nth-of-type(2)': {
-						color: 'black',
-					},
-					':nth-of-type(3)': {
-						color: 'red',
 					},
 				},
 			},
