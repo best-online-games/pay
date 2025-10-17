4f0689ce14af365b195f5cc7b75716b3794b4ea2
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-10-17 11:08:49 +0300

    s

--- app/app.view.ts
+++ app/app.view.ts
@@ -1,7 +1,10 @@
 namespace $.$$ {
 	export class $bog_pay_app extends $.$bog_pay_app {
 		body() {
-			this.Deck().Switch().title
+			const originalLang = this.$.$mol_locale.lang()
+			this.$.$mol_locale.lang('en')
+			this.$.$mol_state_arg.value('page', this.Deck().Content().title().replaceAll(' ', '_'))
+			this.$.$mol_locale.lang(originalLang)
 			return [this.Content()]
 		}
 	}
