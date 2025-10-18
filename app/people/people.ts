122eb03d6f2a9f6afb4fe4528edd23ea362171b3
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-10-18 23:50:19 +0300

    fix

--- app/people/people.ts
+++ app/people/people.ts
@@ -8,7 +8,7 @@ namespace $ {
 			// Use shared land so every user appends to the same People list
 			const glob = this.$.$hyoo_crus_glob
 			const shared_land = glob.land_grab({ '': $hyoo_crus_rank_post('just') })
-			const ref = this.$.$hyoo_crus_ref_resolve(shared_land.ref(), this.$.$hyoo_crus_ref('bogPeopl'))
+			const ref = this.$.$hyoo_crus_ref_resolve(shared_land.ref(), this.$.$hyoo_crus_ref('___bogPeopl'))
 			return glob.Node(ref, $bog_pay_app_people)
 		}
 	}
