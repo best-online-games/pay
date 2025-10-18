f53ea049b4817129cf4b04a8f9792fe8019e53fb
Dev-cmyser <105227884+Dev-cmyser@users.noreply.github.com>
2025-10-18 14:44:54 +0000

    Deploying to gh-pages from @ best-online-games/pay@3f606463fc95703e96ce34979453aa6e57cfca96 🚀

--- web.js
+++ web.js
@@ -9997,28 +9997,22 @@ var $;
                 Switch: {
                     justifyContent: 'space-between',
                     Option: {
+                        font: {
+                            size: 'large',
+                            weight: 'bold',
+                        },
                         ':nth-of-type(1)': {
                             color: '#667eea',
-                            font: {
-                                size: 'larger',
-                                weight: 'bold',
-                            },
                             ':hover': {
                                 background: { color: '#667eea00' },
                             },
                         },
-                        ':nth-of-type(2)': {
-                            color: 'black',
-                        },
+                        ':nth-of-type(2)': {},
                         ':nth-of-type(3)': {
                             backgroundImage: 'linear-gradient(135deg, #667eea 0%, #764ba2 100%)',
                             color: 'white',
                             borderRadius: '8px',
                             padding: { top: '12px', right: '24px', bottom: '12px', left: '24px' },
-                            font: {
-                                size: 'large',
-                                weight: 'bold',
-                            },
                         },
                     },
                 },
