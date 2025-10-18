3f606463fc95703e96ce34979453aa6e57cfca96
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-10-18 17:44:06 +0300

    fix

--- app/app.view.css.ts
+++ app/app.view.css.ts
@@ -4,28 +4,22 @@ namespace $.$$ {
 			Switch: {
 				justifyContent: 'space-between',
 				Option: {
+					font: {
+						size: 'large',
+						weight: 'bold',
+					},
 					':nth-of-type(1)': {
 						color: '#667eea',
-						font: {
-							size: 'larger',
-							weight: 'bold',
-						},
 						':hover': {
 							background: { color: '#667eea00' },
 						},
 					},
-					':nth-of-type(2)': {
-						color: 'black',
-					},
+					':nth-of-type(2)': {},
 					':nth-of-type(3)': {
 						backgroundImage: 'linear-gradient(135deg, #667eea 0%, #764ba2 100%)',
 						color: 'white',
 						borderRadius: '8px',
 						padding: { top: '12px', right: '24px', bottom: '12px', left: '24px' },
-						font: {
-							size: 'large',
-							weight: 'bold',
-						},
 					},
 				},
 			},
