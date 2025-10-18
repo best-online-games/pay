c7518f54d988cd5d1676f184066440aee7006cdc
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-10-19 00:14:46 +0300

    fix

--- app/admin/admin.ts
+++ app/admin/admin.ts
@@ -2,18 +2,19 @@ namespace $ {
 	// 1) Список админов по короткому user_id (peer)
 	// Заполняй сюда свои user_id (например: 'u2ldl9lC', 'abc12345', ...)
 	export const $bog_pay_app_admin_peers = [
-		'SjixkGkN',
 		'mcBM6jhX',
+		'SSCOg7yi', // Current user
 		// 'CR9M4ik7',
 		// 'SjixkGkN'
 	] as const
 
 	export class $bog_pay_app_admin extends $mol_object2 {
-		// Является ли текущий пользователь админом (по user_id/peer)
 		@$mol_mem
 		static is_me() {
 			const my_peer = this.$.$hyoo_crus_glob.home().land().auth().peer()
-			return ($bog_pay_app_admin_peers as readonly string[]).includes(my_peer)
+			const is_admin = ($bog_pay_app_admin_peers as readonly string[]).includes(my_peer)
+
+			return is_admin
 		}
 	}
 }
