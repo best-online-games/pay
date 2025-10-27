f0573651bd587cc9636b5b3f899edd5283fdf18e
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-10-27 17:54:53 +0300

    s

--- app/app.view.ts
+++ app/app.view.ts
@@ -1,22 +1,42 @@
 namespace $.$$ {
-	new $mol_after_frame(() => {
-		$hyoo_crus_yard.masters = ['https://crus.hd4.ru/'] // только прод-мастер
-		$hyoo_crus_glob.yard().sync() // дёрнуть синхронизацию
-	})
+  new $mol_after_frame(() => {
+    $hyoo_crus_yard.masters = ['https://crus.hd4.ru/'] // только прод-мастер
+    $hyoo_crus_glob.yard().sync() // дёрнуть синхронизацию
+  })
 
-	export class $bog_pay_app extends $.$bog_pay_app {
-		body() {
-			const originalLang = this.$.$mol_locale.lang()
-			this.$.$mol_locale.lang('en')
-			this.$.$mol_state_arg.value('page', this.Deck().Content().title().replaceAll(' ', '_'))
-			this.$.$mol_locale.lang(originalLang)
-			return [this.Content()]
-		}
-	}
+  export class $bog_pay_app extends $.$bog_pay_app {
+    @$mol_mem
+    domain_id() {
+      return 'OonXNN12_gSræV7iT' as $mol_int62_string
+    }
 
-	export const $bog_pay_person = $.$bog_pay_app_person
-	export const $bog_pay_plan = $.$bog_pay_app_plan
-	export const $bog_pay_subscription = $.$bog_pay_app_subscription
-	export const $bog_pay_account = $.$bog_pay_app_account_domain
-	export const $bog_pay_invoice = $.$bog_pay_app_invoice
+    @$mol_mem
+    domain_rights() {
+      const land_id = this.domain_id()
+      return new Uint8Array($mol_fetch.buffer(require(`/bog/pay/app/${land_id}!${land_id}.bin`)))
+    }
+
+    @$mol_mem
+    Domain() {
+      const yard = super.Yard()
+
+      $mol_wire_sync(yard.world()).apply(this.domain_rights())
+
+      return yard.world().Fund($bog_pay_app_domain).Item(this.domain_id())
+    }
+
+    body() {
+      const originalLang = this.$.$mol_locale.lang()
+      this.$.$mol_locale.lang('en')
+      this.$.$mol_state_arg.value('page', this.Deck().Content().title().replaceAll(' ', '_'))
+      this.$.$mol_locale.lang(originalLang)
+      return [this.Content()]
+    }
+  }
+
+  export const $bog_pay_person = $.$bog_pay_app_person
+  export const $bog_pay_plan = $.$bog_pay_app_plan
+  export const $bog_pay_subscription = $.$bog_pay_app_subscription
+  export const $bog_pay_account = $.$bog_pay_app_account_domain
+  export const $bog_pay_invoice = $.$bog_pay_app_invoice
 }
---
+++ app/domain/domain.ts
@@ -0,0 +1,8 @@
+namespace $ {
+  export class $bog_pay_app_domain extends $hyoo_crowd_struct {
+    @$mol_mem
+    editable() {
+      return this.land.allowed_mod()
+    }
+  }
+}
