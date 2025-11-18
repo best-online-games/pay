ce78604abdb3dfaffa4ebfd0a0646975e6216a92
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-11-19 02:23:19 +0300

    аыв
    Signed-off-by: novgorodcev.k <novgorodcev.k@rwb.ru>

--- app/admin/admin.view.ts
+++ app/admin/admin.view.ts
@@ -1,14 +1,6 @@
 namespace $.$$ {
-	// 1) Список админов по короткому user_id (peer)
-	// Заполняй сюда свои user_id (например: 'u2ldl9lC', 'abc12345', ...)
-	export const $bog_pay_app_admin_peers = [
-		'oUduJQXl',
-		'mcBM6jhX',
-		'SSCOg7yi', // Current user
-		// 'CR9M4ik7',
-		// 'SjixkGkN'
-	] as const
-	// Admin page: shows list of users and runs "cron-like" enforcement while open.
+	export const $bog_pay_app_admin_peers = ['oUduJQXl', 'mcBM6jhX'] as const
+
 	export class $bog_pay_app_admin extends $.$bog_pay_app_admin {
 		@$mol_mem
 		is_admin() {
